c2ee0a5c518855241340605246d4fa2781c5fd24
Rupali Singh <rupalisingh2304@gmail.com>
2021-08-15 04:09:46 +0530

    Initial commit

--- src/Components/Post.js
+++ src/Components/Post.js
@@ -1,70 +1,10 @@
-import React, { useState, useEffect } from 'react'
-import { database } from '../firebase';
-import { Avatar, CircularProgress } from '@material-ui/core'
-import Video from './Video';
+import React from 'react'
 
-
-function Post({ userData = null }) {
-    const [posts, Setposts] = useState(null)
-    const callback = entries => {
-        entries.forEach(element => {
-            console.log(element)
-            let el = element.target.childNode[0]
-            el.play().then(() => {
-                if (!el.paused && !element.isIntersecting) {
-                    el.pause()
-                }
-            })
-        })
-    }
-    const observer = new IntersectionObserver(callback, { threshold: 0.9 })
-    useEffect(() => {
-        let postarr = []
-        const unsub = database.posts.orderBy('createdAt', 'desc').onSnapshot(querySnapshot => {
-            postarr = []
-            querySnapshot.forEach((doc) => {
-                console.log(doc.data())
-                let data = { ...doc.data(), postId: doc.id }
-                postarr.push(data)
-            })
-            Setposts(postarr)
-        })
-        return unsub
-    }, [])
-    useEffect(() => {
-let elements = document.querySelectorAll('.videos')
-elements.forEach(el => {
-    observer.observe(el)
-})
-return () => {
-    observer.disconnect()
-}
-    }, [posts])
+function Post() {
     return (
-        <>
-            <div className='places'>
-
-            </div>
-            {
-                posts == null ? <CircularProgress color="secondary" /> : <>
-                    <div className='video-container'>
-                        {
-                            posts.map((post, index) => (
-                                <React.Fragment key={post.postId}>
-                                    <div className='videos'>
-                                        <Video source={post.pUrl} id={post.pid}></Video>
-                                    <div className = 'fa'>
-                                        <Avatar src = {post.uProfile}></Avatar>
-                                    <h4>{post.uname}</h4>
-                                    </div>
-                                    </div>
-                                </React.Fragment>
-                            ))
-                        }
-                    </div>
-                </>
-            }
-        </>
+        <div>
+            
+        </div>
     )
 }
 
