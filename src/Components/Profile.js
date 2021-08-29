749a7d210231b84a2c7d3a56f460350728e9b2f5
Rupali Singh <rupalisingh2304@gmail.com>
2021-08-29 17:06:28 +0530

    Completed profile pagel

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -1,5 +1,5 @@
 import React, { useState, useContext, useEffect } from 'react'
-import { Paper, Avatar, CircularProgress, makeStyles } from '@material-ui/core'
+import { createTheme, Backdrop, Modal, Fade, Paper, Avatar, CircularProgress, makeStyles } from '@material-ui/core'
 import { AuthContext } from '../Context/AuthProvider'
 import "./Profile.css"
 import styled, { ThemeProvider } from "styled-components";
@@ -9,6 +9,11 @@ import Header from './Header'
 import Video from './Video'
 import Likes from './Likes'
 import ChatBubbleOutlineRoundedIcon from '@material-ui/icons/ChatBubbleOutlineRounded';
+import Comments from './Comments'
+import AddComments from './AddComments';
+import './OpenPost.css'
+import './backdrop.css'
+import { teal } from '@material-ui/core/colors';
 
 
 const useStyles = makeStyles({
@@ -34,11 +39,16 @@ const useStyles = makeStyles({
         border: "1px solid red",
 
     },
-    actionbtns: {
-        position: 'absolute',
-        zIndex: "100",
 
-    }
+    PostModal: {
+        height: "70%",
+        width: "60%",
+        position: 'absolute',
+        marginLeft: '20%',
+        marginTop: '10%',
+        display: 'flex',
+
+    },
 
 })
 
@@ -52,6 +62,14 @@ function Profile() {
       height: ${theme.spacing(size)}px; 
     `};
   `;
+
+    const theme1 = createTheme({
+        palette: {
+            primary: {
+                main: teal[500],
+            },
+        },
+    });
 
     const { currentUser } = useContext(AuthContext)
     const [ProfileuserData, setProfileUserData] = useState(null)
@@ -69,12 +87,12 @@ function Profile() {
 
     useEffect(() => {
         let newarr = []
-        if(ProfileuserData){
+        if (ProfileuserData) {
             const sub = database.posts.orderBy("createdAt", "desc").onSnapshot(querysnapshot => {
                 newarr = []
-                querysnapshot.forEach((doc)=> {
+                querysnapshot.forEach((doc) => {
                     console.log(doc.data())
-                    let data = {...doc.data(), postId : doc.id}
+                    let data = { ...doc.data(), postId: doc.id }
                     newarr.push(data)
                 })
                 setvideourl(newarr)
@@ -119,21 +137,57 @@ function Profile() {
                             {
                                 videourl.map((post, index) => (
                                     <React.Fragment>
-                                        <Paper className="AllPost" elevation={5}>
-                                            <Video source={post.pUrl} />
-                                            <div className={classes.actionbtns}>
-                                                <ChatBubbleOutlineRoundedIcon fontSize="medium" onClick={() => ModalOpen(post.pid)} />
-                                                <Likes userData={ProfileuserData} postData={post} />
-                                            </div>
-                                        </Paper>
-                                    </React.Fragment>
-                                ))
+                                            <Paper className="AllPost" elevation={5}>
+                                                <div className= 'actionbtns'>
+                                                    <ChatBubbleOutlineRoundedIcon fontSize="medium" onClick={() => ModalOpen(post.pid)} />
+                                                    <Likes userData={ProfileuserData} postData={post} />
+                                                </div>
+                                                <Video source={post.pUrl} />
+                                            </Paper>
+                                            {
+                                                
+                                                <ThemeProvider theme={theme1}>
+                                                < Modal
+                                                    className={classes.modal}
+                                                    open={OpenModal === post.pid}
+                                                    onClose={ModalClose}
+                                                    BackdropComponent={Backdrop}
+                                                    BackdropProps={{
+                                                        timeout: 500,
+                                                    }}
 
+                                                ><Fade in={isModalOpened}>
+                                                        <Paper className={classes.PostModal} elevation={4}>
+                                                            <div className='col-6'>
+                                                                <div className="video-part">
+                                                                    <Video source={post.pUrl}></Video>
+                                                                </div>
+                                                            </div>
+                                                            <div className="col-6">
+                                                                <div className="info-part">
+                                                                    <div className="user-info">
+                                                                        <Avatar src={post.uProfile} className="profilepic"></Avatar>
+                                                                        <h4 className="user-name">{post.uname}</h4>
+                                                                    </div>
+                                                                    <div className="comments-history">
+                                                                        <Comments postData={post}></Comments>
+                                                                    </div>
+                                                                    <AddComments userData={ProfileuserData} postData={post} ></AddComments>
+                                                                </div>
+                                                            </div>
+                                                        </Paper>
+                                                    </Fade>
+                                                </Modal>
+                            </ThemeProvider>
+                                            }
+                                        </React.Fragment>
+                                ))
                             }
                         </div>
+
                     </>
             }
-           
+
         </>
     )
 }
